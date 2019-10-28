d67db4a26b2bbb4d7f6e31b4dbe68a1eb71c5fbb
Jay Chai <chunsaa@gmail.com>
2019-10-28 05:31:16 +0000

    added delete

--- express_server.js
+++ express_server.js
@@ -52,8 +52,7 @@ app.post("/urls", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
   let randomURL = generateRandomString();
   urlDatabase[randomURL] = `http://${req.body.longURL}`;
-  let templateVars = { shortURL: randomURL, longURL: req.body.longURL };
-  res.render("urls_show", templateVars);
+  res.redirect("/urls");
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
@@ -62,6 +61,10 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
